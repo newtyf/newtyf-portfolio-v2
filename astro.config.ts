aeaa9537411f533a1bb792a77099ec86ba0e4620
newt <axelskam.123@outlook.es>
2023-05-30 22:15:45 -0500

    compress images, remove compress

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), compress()]
+  integrations: [react()]
 });
