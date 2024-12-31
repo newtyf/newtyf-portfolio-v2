fe2b18c1ff2ea0295ccfb8c9e9a56f6e244fe05e
newt <axelskam.123@outlook.es>
2024-12-31 11:28:55 -0500

    feat: :sparkles: Add SSR integration with cloudflare and Contact Form
    Add SSR integration with cloudflare and Contact Form with Resend and server endpoints

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,14 @@
 /// <reference types="astro/client" />
+
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    otherLocals: {
+      test: string;
+    };
+  }
+}
 interface Window {
   dataLayer: any[];
   particlesJS: {
